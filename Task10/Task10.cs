cb7e188c2a6e08956762da3d08d8058cc4df66e8
AlexKhabibov <alex.lv.u@gmail.com>
2023-06-15 22:12:28 +0300

    added homeworksem2

---
+++ Task10/Task10.cs
@@ -0,0 +1,9 @@
+﻿// Задача 10. Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Input 3x number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int number2 = number % 100 / 10;
+
+Console.WriteLine("Your number is " + number2);
---
+++ Task13/Task13.cs
@@ -0,0 +1,21 @@
+﻿// Задача 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Input number"); 
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Your number is " + number);
+if (number < 1000)
+{
+    if (number > 99)
+    {
+    number = number % 10; 
+    Console.WriteLine("Your number is " + number); 
+    }
+    else
+    {
+    Console.WriteLine("Youre number has no third figure");
+    }
+}
+else
+{
+    Console.WriteLine("Incorrect input");
+}
